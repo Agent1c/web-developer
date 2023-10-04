b13686b23276a63940d3003309f771d578e33885
‘Agent1c’ <medicenyb@gmail.com>
2023-10-04 20:23:35 +0200

    Task #2 - I tried  to work on  it, to  al,ost the point i  want it to   be.

---
+++ cv_html/css/check_file.c
@@ -0,0 +1,17 @@
+int main()
+{
+    float latitude;
+    float longitude;
+    char info[80];
+    int started = .............;
+
+    puts("data =[]");
+    while (scanf("%f, %f, %79[^\n]", ............., ............. , .............)==3){
+        if(started)
+        printf(",\n");
+        else
+        started = .............;
+        printf("{latitude: %f, info: '%s}", ............. , ............. ,.......);
+    puts("\n");
+    return (0);
+    }
